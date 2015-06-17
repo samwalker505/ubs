b2852fc4795dbc7cb831181356ee1e542c31082b
samwalker <samwalker505@gmail.com>
2015-06-18 01:17:40 +0800

    added ubs main page

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var ubsApp =  angular.module('ubsApp', ['ionic']);
 
-.run(function($ionicPlatform) {
+ubsApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +16,19 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+ubsApp.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/login');
+
+	$stateProvider
+		.state('login', {
+			url: '/login',
+			templateUrl: 'views/login.html'
+		})
+		.state('main', {
+			url:'/main',
+			templateUrl: 'views/main.html'
+		});
+
+});
